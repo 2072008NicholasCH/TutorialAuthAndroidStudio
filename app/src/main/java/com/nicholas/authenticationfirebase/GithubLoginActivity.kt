9ead7200434db3de650779dd258e92e380bb1b28
Nicholas CH <2072008@maranatha.ac.id>
2022-12-30 08:04:23 +0700

    First Commit

---
+++ app/src/main/java/com/nicholas/authenticationfirebase/GithubLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.nicholas.authenticationfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GithubLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_github_login)
+    }
+}
